b70284bf712f9a25c020c4fc3616e143e9a6d421
Alvrus <jack.alvrus@appliedis.com>
2019-08-08 16:20:47 -0400

    after refactoring

--- HttpEnv/Content.cs
+++ HttpEnv/Content.cs
@@ -5,11 +5,33 @@ namespace HttpEnv
 {
     public class Content
     {
+        private readonly IUrlMapper _mapper;
+
+        public Content(IUrlMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
+        public Content() : this(new HostingEnvironmentUrlMapper()) { }
+
         public string Shout(string localUrlPath)
         {
-            string physicalPath = HostingEnvironment.MapPath(localUrlPath);
+            string physicalPath = _mapper.Map(localUrlPath);
             string content = File.ReadAllText(physicalPath);
-            return content.ToUpper();
+            return content.ToUpper() + "!";
+        }
+    }
+
+    public interface IUrlMapper
+    {
+        string Map(string localUrlPath);
+    }
+
+    public class HostingEnvironmentUrlMapper : IUrlMapper
+    {
+        public string Map(string localUrlPath)
+        {
+            return HostingEnvironment.MapPath(localUrlPath);
         }
     }
 }
--- HttpEnvTests/ContentTests.cs
+++ HttpEnvTests/ContentTests.cs
@@ -1,6 +1,9 @@
 ﻿using HttpEnv;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.IO;
+using System.Reflection;
+using System.Web.Hosting;
 
 namespace HttpEnvTests
 {
@@ -18,13 +21,62 @@ namespace HttpEnvTests
 
             string url = "/" + fileName;
 
+            var test = new Content(new MockUrlMapper(filePath));
+            
+            string result = test.Shout(url);
+
+            Assert.AreEqual(content.ToUpper() + "!", result);
+        }
+
+        public class MockUrlMapper : IUrlMapper
+        {
+            private readonly string _filePath;
+
+            public MockUrlMapper(string filePath)
+            {
+                _filePath = filePath;
+            }
+
+            public string Map(string localUrlPath)
+            {
+                return _filePath;
+            }
+        }
+
+        [TestMethod]
+        public void ShoutIntegrationTest()
+        {
+            string fileName = "bar.txt";
+
+            string filePath = Path.Combine(Path.GetTempPath(), fileName);
+            string content = "original content";
+            File.WriteAllText(filePath, content);
+
+            string url = "/" + fileName;
+
             var test = new Content();
 
-            //TODO: somehow get HostingEnvironment.MapPath to convert url to filePath
+            MockMapPath(url, filePath);
 
             string result = test.Shout(url);
 
-            Assert.AreEqual(content.ToUpper(), result);
+            Assert.IsNotNull(result);
+        }
+
+        private void MockMapPath(string url, string filePath)
+        {
+            var hostingEnvironment = new HostingEnvironment();
+            var vpType = typeof(HostingEnvironment).Assembly.GetType("System.Web.VirtualPath");
+            var createMethod = vpType.GetMethod("Create", new[] { typeof(string) });
+            var virtualPath = createMethod.Invoke(null, new[] { url });
+            SetPrivateField(hostingEnvironment, "_appVirtualPath", virtualPath);
+            SetPrivateField(hostingEnvironment, "_appPhysicalPath", filePath);
+        }
+
+        private void SetPrivateField(HostingEnvironment hostingEnvironment, string fieldName, object value)
+        {
+            var fieldInfo = hostingEnvironment.GetType().GetField(fieldName, BindingFlags.Instance | BindingFlags.NonPublic);
+            fieldInfo.SetValue(hostingEnvironment, value);
         }
     }
 }
